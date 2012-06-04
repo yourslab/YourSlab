e2cdfce4bb9d00cd5631c9e19e0b78d99105b789
Oscar Abrina <admin@yourslab.com>
2012-06-04 13:47:32 +0800

    Updated the layout of the Homepage and fixed some CSS glitches

--- application/modules/site/views/home.php
+++ application/modules/site/views/home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
 		<title>
 			YourSlab - YourSlab is a universal meeting place for all types of user-groups. Want to share something? Login or signup today.
 		</title>
@@ -14,9 +14,12 @@
 		<link href="<?php echo base_url(); ?>css/bootstrap-responsive.css" rel="stylesheet" />
 		<link href="<?php echo base_url(); ?>css/scrollable-horizontal.css" rel="stylesheet" />
 		<link href="<?php echo base_url(); ?>css/scrollable-buttons.css" rel="stylesheet" />
+        <link href="<?php echo base_url(); ?>css/jquery.qtip.min.css" rel="stylesheet" />
+        
         
         <!-- Include Fonts here -->
         <link href='http://fonts.googleapis.com/css?family=Contrail+One' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css' />
 		<!-- IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
@@ -65,7 +68,8 @@
 				<div class="row">
 					<div class="span2">
 						<p>
-							<a class="btn btn-warning btn-large signup" style="width:200px;height:50px;font-size:40px;" href="<?php echo base_url(); ?>register/step1">Sign Up &raquo;</a>
+							<a class="btn btn-warning btn-large signup" style="width:200px;height:50px;font-size:40px;" href="<?php echo
+base_url(); ?>register/step1">Sign Up &raquo;</a>
 						</p>
 					</div>
 					<div class="span3 offset5">
@@ -104,30 +108,30 @@
 							<div class="items">
 								<!-- 1-5 -->
 								<div>
-									<img src="http://farm1.static.flickr.com/143/321464099_a7cfcb95cf_t.jpg" />
-									<img src="http://farm4.static.flickr.com/3089/2796719087_c3ee89a730_t.jpg" />
-									<img src="http://farm1.static.flickr.com/79/244441862_08ec9b6b49_t.jpg" />
-									<img src="http://farm1.static.flickr.com/28/66523124_b468cf4978_t.jpg" />
-									<img src="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" />
-									<img src="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" />
+									<img class="lazy" data-original="http://farm1.static.flickr.com/143/321464099_a7cfcb95cf_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm4.static.flickr.com/3089/2796719087_c3ee89a730_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/79/244441862_08ec9b6b49_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/28/66523124_b468cf4978_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
 								</div>
 								<!-- 5-10 -->
 								<div>
-									<img src="http://farm1.static.flickr.com/163/399223609_db47d35b7c_t.jpg" />
-									<img src="http://farm1.static.flickr.com/135/321464104_c010dbf34c_t.jpg" />
-									<img src="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" />
-									<img src="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" />
-									<img src="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" />
-									<img src="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" />
+									<img class="lazy" data-original="http://farm1.static.flickr.com/163/399223609_db47d35b7c_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/135/321464104_c010dbf34c_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
 								</div>
 								<!-- 10-15 -->
 								<div>
-									<img src="http://farm4.static.flickr.com/3629/3323896446_3b87a8bf75_t.jpg" />
-									<img src="http://farm4.static.flickr.com/3023/3323897466_e61624f6de_t.jpg" />
-									<img src="http://farm4.static.flickr.com/3650/3323058611_d35c894fab_t.jpg" />
-									<img src="http://farm4.static.flickr.com/3635/3323893254_3183671257_t.jpg" />
-									<img src="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" />
-									<img src="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" />
+									<img class="lazy" data-original="http://farm4.static.flickr.com/3629/3323896446_3b87a8bf75_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm4.static.flickr.com/3023/3323897466_e61624f6de_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm4.static.flickr.com/3650/3323058611_d35c894fab_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm4.static.flickr.com/3635/3323893254_3183671257_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/40/117346184_9760f3aabc_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
+									<img class="lazy" data-original="http://farm1.static.flickr.com/153/399232237_6928a527c1_t.jpg" src="<?php echo base_url();?>img/grey.gif"/>
 								</div>
 							</div>
 						</div>
@@ -136,14 +140,70 @@
 					</div>
 				</div>
 			</div>
+                <div class="row"  style="margin-top: 20px; margin-bottom:30px;">
+                <div class="span2">
+                <div class="suggestedslabbox">
+                <div class="slabcontent">
+                asdf
+                </div>
+                </div>
+                </div>
+                <div class="span10">
+                <div class="slabactivitybox">
+                <div class="slabcontent">
+                <?php
+                $i = 1;
+                while ($i <= 4) {
+                    $i++;
+                ?>
+                <?php if($i==5){ ?>
+                <div class="slabsinslabactivity" style="border: none;">
+                <?php } else {?>
+                <div class="slabsinslabactivity">
+                <?php } ?>
+                <div class="image-box" title="<strong>5 Members<br />5 Posts<br />5 Subslabs</strong>">
+                <img src="http://farm8.staticflickr.com/7112/7080624215_939d8eff86_m.jpg" width="156px" height="104px"/>
+                <div id="overlay"><p>Flower Slab</p></div>
+                </div>
+                </div>
+                <div class="slabsinactivitycontent">
+                <?php 
+                for ($n=1;$n<=4;$n++) {
+                ?>
+                <div class="slabactivitylist">
+                <div class="customheading">What the Flower Slab is about and why we created it.</div>
+                <div class="smallcaption">
+                - $username commented on the Flower Power Category
+                </div>
+                </div>
+                <?php
+                }
+                ?>
+                </div>
+                <?php } ?>
+                </div>
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span1">
+                <div class="suggestedslabsheader">
+                    <h2 style="padding-left: 52px; padding-top: 6px;">Suggested Slabs</h2>
+                </div>
+                </div>
+                <div class="span5">
+                <div class="slabactivityheader">
+                <h2 style="padding-left: 22px; padding-top: 6px;">Slab Activity</h2>
+                </div>   
+                </div>
+            </div>
             </div>
             <div class="push"><!--//--></div>
             </div>
 			<footer>
 				<div class="container">
 					Page loaded in
-					<?php echo $this->
-						benchmark->elapsed_time(); ?> seconds.
+					<?php echo $this->benchmark->elapsed_time(); ?> seconds.
 				</div>
 			</footer>
 		<!-- /container -->
@@ -161,6 +221,9 @@
 			
 		
 		</script>
+        <script src="<?php echo base_url(); ?>js/jquery.lazyload.min.js">
+        
+        </script>
 		<script src="<?php echo base_url(); ?>js/html5placeholder.jquery.js">
 			
 		
@@ -169,9 +232,35 @@
 			
 		
 		</script>
-		<script type="text/javascript">
+        <script src="<?php echo base_url(); ?>js/jquery.qtip.min.js">
 			
+		
+		</script>
+        <script type="text/javascript">
+        $(document).ready(function()
+        {
+            $('.image-box[title]').qtip({
+               show: {
+                event:false,
+                ready:true
+               },
+               position: {
+               my: 'top center',
+               at: 'bottom center'
+               },
+               style: 'ui-tooltip-shadow ui-tooltip-yellow',
+               hide: false
+            }); 
+        });
+        </script>
+		<script type="text/javascript">	
+        		
 			$('input').placeholder();
+            
+            $("img").lazyload({
+                effect : "fadeIn",
+                container: $(".items")
+            });
 		
 		</script>
 		<script type="text/javascript">
